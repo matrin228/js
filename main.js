a6031fb673505c6999e9c563bc4228c909ffa3fa
matrin228 <denismartinov12@gmail.com>
2022-07-22 11:33:23 +0000

    js

--- main.js
+++ main.js
@@ -3,3 +3,14 @@ function stringToArray(string){
     return string.split(' ');
   }
 //№2
+function DNAtoRNA(dna) {
+  return dna.split("T").join("U");
+}
+//№3
+var min = function(list){   
+  return Math.min(...list);
+}
+var max = function(list){  
+  return Math.max(...list);
+}
+//№4
